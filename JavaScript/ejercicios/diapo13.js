7a4ad77bce38c4f1cbd25df3cb0b4fc378625ecf
Bryancc97 <bryan.carrera.7e8@itb.cat>
2025-01-22 12:51:05 +0100

    ff

---
+++ JavaScript/ejercicios/diapo13.js
@@ -0,0 +1,13 @@
+/*Ejercicio 1*/
+function mayor (num1, num2){
+    if(num1 > num2) {
+        console.log(`${num1} es mayor que ${num2}`);
+    } else if (num2 > num1){
+        console.log(`${num2} es mayor que ${num1}`);
+    } else {
+        console.log (`Ambos numeros son iguales`);
+    }
+}
+mayor(10, 20);
+
+/*Ejercicio 2*/
