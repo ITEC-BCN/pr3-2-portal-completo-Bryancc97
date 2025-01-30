ee48e6b07dea8f9fe057c7c2b1d7486a4322ac51
Bryan C <bryan.carrera.7e8@itb.cat>
2025-01-30 09:39:53 +0100

    mod

--- JavaScript/ejercicios/diapo72.js
+++ JavaScript/ejercicios/diapo72.js
@@ -8,4 +8,12 @@ tvSamsung.getImporte = function(){
     return 'Unidades: ' + this.unidades + ', precio/u: ' + this.precio + ', importe total: ' + this.unidades * this.precio;
   }
 
+tvSamsung.getImporte2 = function(){
+    return this.unidades * this.precio;
+  }
+const importeTotalElement = document.getElementById('importe-total'); // Acceder al elemento HTML
+importeTotalElement.textContent = tvSamsung.getImporte2();
+
 console.log(tvSamsung.getImporte()); 
+console.log(tvSamsung.getImporte2());
+ 
