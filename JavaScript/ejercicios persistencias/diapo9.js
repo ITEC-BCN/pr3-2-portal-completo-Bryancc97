47ae6c727b5354e9db7d9d4cea64e4b9cd8ce23a
Bryan C <bryan.carrera.7e8@itb.cat>
2025-03-13 09:33:39 +0100

    sin terminar

--- JavaScript/ejercicios persistencias/diapo9.js
+++ JavaScript/ejercicios persistencias/diapo9.js
@@ -1,8 +1,41 @@
-document.cookie = "nombre=Juan";
+//pidele el nombre (prompt) y guardalo en una cookie, si la cookie existe lee la cookie y saludalo por el nombre
+function init() {
+    var nombre = prompt("Introduce tu nombre");
+    document.cookie = "nombre=" + nombre;
+    if(document.cookie != ""){
+        var cookie = document.cookie;
+        var arrayCookie = cookie.split("=");
+        alert("Hola " + arrayCookie[1]);
+    }
+}
+init();
+
+
+
+
+
+
+
+
+
+
+
+
+/*document.cookie = "nombre=Juan";
 document.cookie = "apellido=Perez";
 function alertCookie(){
     alert(document.cookie);
 }
 alertCookie();
 
+function init() {
+    if(document.cookie !=""){
+        alertCookie()
+    }else {
+        document.cookie = "nombre=Juan";
+        document.cookie = "apellido=Perez";
+    }
 
+}
+
+console.log(document.cookie);*/
